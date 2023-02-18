1b1067b32bbdfaed99667ad7b121b4c1b485f6cd
shashikumar.venkatesh <shashikumar.venkatesh@censanext.com>
2023-02-18 21:23:05 +0530

    Update

--- helpers/common.js
+++ helpers/common.js
@@ -2,26 +2,12 @@ const nodemailer = require('nodemailer');
 
 const sendMail = (to, subject, content) => {
 
-    let fromMail = 'shashikumarvchandru@gmail.com';
-    // create reusable transporter object using the default SMTP transport
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        host: "smtp.gmail.com",
-        port: 465,               // true for 465, false for other ports
-        secure: false,
-        auth: {
-            user: fromMail,
-            pass: 'shashi@19941996',
-        },
-        
-    });
-
     const mailData = {
         from: fromMail,  // sender address
         to: to,   // list of receivers
         subject: subject,
         text: content,
-        html: '<b>Hey there! </b> <br> This is our first message sent with Nodemailer<br/>',
+        //html: '<b>Hey there! </b> <br> This is our first message sent with Nodemailer<br/>',
     };
 
     transporter.sendMail(mailData, function (err, info) {
